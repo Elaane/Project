9a42dfcf32f20799b62073275e887a35f9607e19
Elaane <elin.damgard@skola.taby.se>
2024-05-17 19:20:59 +0200

    sista grejjen kontakt

---
+++ js/kontakt.js
@@ -0,0 +1,19 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+  
+    var name = document.getElementById("namn").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("meddelande").value;
+  
+
+    document.getElementById("bekraftelse").style.display = "block";
+    document.getElementById("bekraftelse").textContent = "Tack för ditt meddelande, " + name + "! Vi återkommer snart.";
+  
+    setTimeout(function() {
+        document.getElementById("contact-form").reset(); 
+        document.getElementById("bekraftelse").style.display = "none"; 
+      }, 3000); 
+});
+  
+
+  
